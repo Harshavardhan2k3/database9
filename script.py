7cbd433e71bbaa58eb001a141c1b2f5e98e5ff54
Harshavardhan <nookaharshavardhan17102003@gmail.com>
2026-02-23 14:43:49 +0530

    Update script.py

--- script.py
+++ script.py
@@ -49,7 +49,7 @@ def fetch_url(request_id):
     except requests.exceptions.RequestException as e:
         return f"[{request_id}] Error for {target_url}: {e}"
 def run_multithreaded_check():
-    total_requests = 10000
+    total_requests = 15000
     max_workers = 500
 
     print(f"Starting execution: {total_requests} requests with up to {max_workers} concurrent threads...")
@@ -66,4 +66,5 @@ def run_multithreaded_check():
 
 if __name__ == "__main__":
     run_multithreaded_check()
+
 
